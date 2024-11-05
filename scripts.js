8bf90c2ce8b1c1e146d3ee5c798bc99755201024
MediocrePancake <aidenrhys2009@outlook.com>
2024-11-04 18:09:09 -0700

    lang test 4

--- scripts.js
+++ scripts.js
@@ -262,7 +262,6 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("registerForm").addEventListener('submit', register);
     document.getElementById("uploadLevelBtn").addEventListener('click', openUploadPanel);
     document.getElementById("uploadLevelForm").addEventListener('submit', uploadLevel);
-    document.getElementById("languageBtn").addEventListener('click', toggleLanguage);
 });
 
 // Listener para cambios en el estado de autenticación
